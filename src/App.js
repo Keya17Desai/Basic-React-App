0f8530daaaceee22c75306a5daa008c51ec3c514
Keya17Desai <keyahdesai2003@gmail.com>
2023-07-04 11:08:50 +0530

    Prac2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
+import Home from "./components/Home/Home";
 import './App.css';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import {BrowserRouter , Routes , Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+    
       <h1>Hello World</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route exact path="/"element={<Home/>} />
+        <Route exact path="/"element={<About/>} />
+        <Route exact path="/"element={<Contact/>} />
+      </Routes>
+      </BrowserRouter>
+    
     </div>
   );
 }
